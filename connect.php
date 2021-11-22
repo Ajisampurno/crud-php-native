aca448963c12814ed9ba39bca4dbaa6227b2da2b
Pelita Nur Najmina <pelitanurnajmina27@gmail.com>
2021-11-22 12:56:39 +0700

    first commit

---
+++ connect.php
@@ -0,0 +1,10 @@
+<?php
+$servername = "localhost";
+$database = "library";
+$username = "root";
+$password = "";
+
+// Create connection
+$mysqli = mysqli_connect($servername, $username, $password, $database);
+
+?>
